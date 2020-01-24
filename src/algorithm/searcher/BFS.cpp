57a25a9a9cffe4568060e873426f8481cc3607b9
idoze2 <ido.ze2@gmail.com>
2020-01-24 20:10:04 +0200

    BFS ALGORITHM

--- src/algorithm/searcher/BFS.cpp
+++ src/algorithm/searcher/BFS.cpp
@@ -2,35 +2,49 @@
 //// Created by allen on 22.1.2020.
 ////
 //
-//#include "BFS.h"
-//namespace searcher
-//{
-//
-//Solution BFS::search(Problem problem)
-//{
-//    clearAll();
-//    Solution solution{};
-//    MatrixNode start = problem.getStart();
-//    MatrixNode goal = problem.getGoal();
-//    marked.push_back(start);
-//    open.push(start);
-//    while (!open.empty())
-//    {
-//        auto *m = new MatrixNode(open.front().getPosition(), open.front().getValue());
-//        if (*m == goal)
-//        {
-//            solution.addStep(*m);
-//        }
-//        for (const auto &neighbor : getNeighbors(problem, *m))
-//        {
-//            if (isUnmarked(neighbor))
-//            {
-//                marked.push_back(neighbor);
-//                open.push(neighbor);
-//            }
-//        }
-//    }
-//    return solution;
-//}
-//
-//} // namespace searcher
+#include "BFS.h"
+
+namespace searcher {
+
+    Solution BFS::search(Problem problem) {
+        clearAll();
+        Solution solution{};
+        MatrixNode start = problem.getStart();
+        MatrixNode goal = problem.getGoal();
+        Position nullPos(-1, -1);
+        MatrixNode nullNode(nullPos, -1);
+        SearcherState s(0, start, nullNode);
+        open.push(s);
+        marked.push_back(start);
+        while (!open.empty()) {
+            auto *m = new SearcherState(0, *(open.front().getNode()), *(open.front().getParent()));
+            open.pop();
+            if (*m->getNode() == goal) {
+                solution.addStep(*m->getNode());
+                MatrixNode *parent = m->getParent();
+                while (!(*parent == nullNode)){
+                    if (*parent == *(searcherState_stack.top().getNode()))
+                    {
+                        solution.addStep(*(searcherState_stack.top().getNode()));
+                        parent = searcherState_stack.top().getParent();
+                        searcherState_stack.pop();
+                    }
+                    else{
+                        searcherState_stack.pop();
+                    }
+                }
+                return solution;
+            } else {
+                for (auto neighbor : getNeighbors(problem, *m->getNode())) {
+                    if (isUnmarked(neighbor)) {
+                        auto *new_state = new SearcherState(0, neighbor, *m->getNode());
+                        open.push(*new_state);
+                        marked.push_back(neighbor);
+                    }
+                }
+                searcherState_stack.push(*m);
+            }
+        }
+        return solution;
+    }
+}
